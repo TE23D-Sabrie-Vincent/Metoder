101758c0d676b4d21fe5636dff54a41251f867db
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-11-22 10:52:59 +0100

    Multi Övning 3

--- Metoder/Program.cs
+++ Metoder/Program.cs
@@ -7,9 +7,15 @@
 
 // hello();
 
-void kvadrat(int num){
-num = num*num;
- System.Console.WriteLine(num); 
- Console.ReadLine();
-} 
-kvadrat(2);
+// void kvadrat(int num){
+// num = num*num;
+//  System.Console.WriteLine(num); 
+//  Console.ReadLine();
+// } 
+// kvadrat(2);
+
+void multi(float num1,float num2){
+System.Console.WriteLine(num1*num2);
+}
+multi(9,2);
+Console.ReadLine();
