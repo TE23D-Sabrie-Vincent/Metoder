4a1eeba3198cb998f140a1c0a6f34466d12b3a37
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-11-25 09:26:12 +0100

    Triangel Övning 4

--- Metoder/Program.cs
+++ Metoder/Program.cs
@@ -14,8 +14,16 @@
 // } 
 // kvadrat(2);
 
-void multi(float num1,float num2){
-System.Console.WriteLine(num1*num2);
+// void multi(float num1,float num2){
+// System.Console.WriteLine(num1*num2);
+// }
+// multi(9,2);
+// Console.ReadLine();
+
+float RightTriangleArea(float num1, float num2){
+    return num1 *num2 / 2;
 }
-multi(9,2);
+
+float area = RightTriangleArea(2,5);
+System.Console.WriteLine(area);
 Console.ReadLine();
